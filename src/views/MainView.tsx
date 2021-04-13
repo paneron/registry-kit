bac1716cd84acff8d1c1001b2500c5c088816414
Anton Strogonoff <anton@strogonoff.name>
2021-04-13 15:53:39 +0300

    chore(MainView): support the style prop

--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -9,13 +9,15 @@ import { css, jsx } from '@emotion/core';
 export const MainView: React.FC<{
   actions?: JSX.Element
   wrapperClassName?: string
+  style?: React.CSSProperties
 }> = function ({
   actions,
   children,
   wrapperClassName,
+  style,
 }) {
   return (
-    <div css={css`flex: 1; display: flex; flex-flow: column nowrap; overflow: hidden;`} className={wrapperClassName}>
+    <div css={css`flex: 1; display: flex; flex-flow: column nowrap; overflow: hidden;`} className={wrapperClassName} style={style}>
       <main css={css`flex: 1; display: flex; flex-flow: row nowrap; align-items: stretch; overflow: hidden;`}>
         {children}
       </main>
