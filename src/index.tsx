93d2611ea57003fa5ce932fa70358deaac87f273
Anton <anton@strogonoff.name>
2023-09-20 17:26:03 +0800

    refactor: provide default export

--- src/index.tsx
+++ src/index.tsx
@@ -32,3 +32,20 @@ export const makeRegistryExtension: RegistryExtensionMaker = function (opts) {
     datasetInitializer,
   });
 };
+
+
+import { BrowserCtx } from './views/BrowserCtx';
+import { itemRefToItemPath, incompleteItemRefToItemPathPrefix } from './views/itemPathUtils';
+import GenericRelatedItemView from './views/GenericRelatedItemView';
+import { PropertyDetailView } from './views/util';
+import CRITERIA_CONFIGURATION from './views/FilterCriteria/CRITERIA_CONFIGURATION';
+
+export default {
+  makeRegistryExtension,
+  itemRefToItemPath,
+  incompleteItemRefToItemPathPrefix,
+  BrowserCtx,
+  CRITERIA_CONFIGURATION,
+  GenericRelatedItemView,
+  PropertyDetailView,
+};
