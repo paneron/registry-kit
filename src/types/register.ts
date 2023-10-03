e72a4d30b02875f6a24469296349cb3da7b87c0b
Anton <anton@strogonoff.name>
2023-10-03 15:48:46 +0800

    fix: treat register meta w/o owners as valid

--- src/types/register.ts
+++ src/types/register.ts
@@ -29,8 +29,8 @@ export function isRegisterMetadata(val: any): val is Register {
     val.stakeholders.length > 0 &&
     // Stakeholders must be right
     val.stakeholders.every(isRegisterStakeholder) &&
-    // Must have at least an owner
-    val.stakeholders.some((s: RegisterStakeholder) => s.role === 'owner') &&
+    // Must have at least an owner? No
+    // val.stakeholders.some((s: RegisterStakeholder) => s.role === 'owner') &&
     // Must have a valid version (or no version? hmm)
     (val.version === undefined || isVersion(val.version)));
 }
