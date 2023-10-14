da2d7077a433704c00158c85bcd54e73a28a8c69
Anton <anton@strogonoff.name>
2023-10-14 17:59:36 +0800

    refactor: `InlineDiffGeneric` helper

--- src/views/diffing/InlineDiff.tsx
+++ src/views/diffing/InlineDiff.tsx
@@ -5,6 +5,8 @@ import { jsx } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom/server';
 import VisualDiff from 'react-visual-diff';
+import { UL } from '@blueprintjs/core';
+import { objectsHaveSameShape, normalizeObjectRecursively } from '@riboseinc/paneron-extension-kit/util';
 import type { InternalItemReference, RegisterItem, Payload } from '../../types/item';
 import { ItemDetailView } from '../../types/views';
 import AnnotatedChange from '../AnnotatedChange';
@@ -40,6 +42,57 @@ const InlineDiff: React.FC<{
 }, (prevProps, nextProps) =>
   JSON.stringify(prevProps.item2) === JSON.stringify(nextProps.item2) &&
   JSON.stringify(prevProps.item1) === JSON.stringify(nextProps.item1)
+);
+
+
+export const Val: React.VoidFunctionComponent<{ val: any }> = function ({ val }) {
+  if (Array.isArray(val)) {
+    return <UL>
+      {val.map((v, idx) =>
+        <li key={idx}><Val val={v} /></li>
+      )}
+    </UL>;
+  } else if (isObject(val)) {
+    return <UL>
+      {Object.entries(val).sort().map(([key, val]) =>
+        <li key={key}><code>{key}</code>: <Val val={val} /></li>
+      )}
+    </UL>;
+  } else {
+    return <code>{val?.toString() ?? '—'}</code>;
+  }
+};
+
+
+function isObject(val: unknown): val is Record<string, any> {
+  return val !== null && typeof val === 'object' && !Array.isArray(val);
+}
+
+
+export const InlineDiffGeneric: React.FC<{
+  item1: Record<string, any>
+  item2: Record<string, any>
+  className?: string
+}> = React.memo(function ({ item1, item2, className }) {
+  const left = 
+    <Val val={normalizeObjectRecursively(item1)} />;
+  const right =
+    <Val val={normalizeObjectRecursively(item2)} />;
+
+  return (
+    <div className={className}>
+      <VisualDiff
+        left={left}
+        right={right}
+        //left={<Val val={normalizeObjectRecursively(item1)} />}
+        //right={<Val val={normalizeObjectRecursively(item2)} />}
+        renderChange={AnnotatedChange}
+      />
+    </div>
+  );
+}, (prevProps, nextProps) =>
+  objectsHaveSameShape(prevProps.item2, nextProps.item2) &&
+  objectsHaveSameShape(prevProps.item1, nextProps.item1)
 );
 
 
