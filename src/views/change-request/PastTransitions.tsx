42d52e34894dab8341f080412e6ddcdd37d6c054
Anton <anton@strogonoff.name>
2023-10-19 20:26:13 +0800

    refactor(PastTransitions): render `pastTransitions`

--- src/views/change-request/PastTransitions.tsx
+++ src/views/change-request/PastTransitions.tsx
@@ -3,12 +3,74 @@
 
 import { jsx, css } from '@emotion/react';
 import React from 'react';
-import { FormGroup, Divider, Classes } from '@blueprintjs/core';
+import { FormGroup, Divider, Classes, Colors } from '@blueprintjs/core';
 import * as CR from '../../types/cr';
+import { STATE_COLOR } from './TransitionOptions';
 
 
+export function getTransitionHistory(cr: CR.Base):
+[key: string, details: JSX.Element | null, color: typeof Colors[keyof typeof Colors] | undefined][] {
+  const els:
+  [key: string, details: JSX.Element | null, color: typeof Colors[keyof typeof Colors] | undefined][] =
+  [];
 
-function getPastTransitions(cr: CR.Base): [key: string, el: JSX.Element][] {
+  if (cr.pastTransitions && cr.pastTransitions.length > 0) {
+
+    els.push(['started', null, undefined]);
+
+    for (const entry of cr.pastTransitions) {
+      const input = entry.input?.toString()?.trim() || null;
+      els.push([
+        entry.label,
+        input ? <>{input}</> : null,
+        STATE_COLOR[entry.toState],
+      ]);
+    }
+
+  } else {
+
+    // Backward compatibility
+
+    els.push(['started', null, undefined]);
+
+    if (CR.isProposed(cr)) {
+      els.push(['proposed', <>{cr.justification}</>, Colors.BLUE1]);
+    }
+
+    if (CR.isSubmittedForControlBodyReview(cr)) {
+      els.push(['submitted for control body review', <>{cr.registerManagerNotes}</>, Colors.BLUE1]);
+    }
+
+    if (CR.isReturnedForClarification(cr)) {
+      els.push(['returned for clarification', <>{cr.registerManagerNotes} <br /> {(cr as any).controlBodyNotes}</>, Colors.ORANGE1]);
+    }
+
+    if (CR.isWithdrawn(cr)) {
+      els.push(['withdrawn', null, undefined]);
+    } else if (CR.isAccepted(cr)) {
+      els.push(['accepted', <>{cr.controlBodyNotes}</>, Colors.GREEN1]);
+    } else if (CR.isRejected(cr)) {
+      els.push(['rejected', <>{cr.controlBodyNotes}</>, Colors.RED1]);
+    }
+
+    if (CR.isAppealed(cr)) {
+      els.push(['appealed', <>{cr.appealReason}</>, Colors.ORANGE1]);
+    }
+
+    if (CR.isRejectedWithAppealWithdrawn(cr)) {
+      els.push(['appeal withdrawn', null, undefined]);
+    } else if (CR.isAcceptedOnAppeal(cr)) {
+      els.push(['accepted on appeal', <>{cr.registerOwnerNotes}</>, Colors.GREEN1]);
+    } else if (CR.isRejectedOnAppeal(cr)) {
+      els.push(['rejection upheld on appeal', <>{cr.registerOwnerNotes}</>, Colors.RED1]);
+    }
+  }
+
+  return els;
+}
+
+
+function getNotes(cr: CR.Base): [key: string, el: JSX.Element][] {
   const els: [key: string, el: JSX.Element][] = [];
   if (CR.hasSubmitterInput(cr)) {
     els.push(['Submitter’s justification', <>{cr.justification}</>]);
@@ -31,7 +93,7 @@ function getPastTransitions(cr: CR.Base): [key: string, el: JSX.Element][] {
 
 const PastTransitions: React.FC<{ cr: CR.Base }> = function ({ cr }) {
   return <>
-    {getPastTransitions(cr).map(([key, el], idx) =>
+    {getNotes(cr).map(([key, el], idx) =>
       <React.Fragment key={key}>
         {idx !== 0 ? <Divider /> : null}
         <FormGroup label={`${key}:`}>
