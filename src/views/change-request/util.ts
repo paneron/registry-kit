54895d97dfcf870090e159c7aa94f4183c73f737
Anton <anton@strogonoff.name>
2023-11-09 19:20:06 +0800

    refactor(cr): helper to get tag props from proposed change

---
+++ src/views/change-request/util.ts
@@ -0,0 +1,21 @@
+import { type TagProps } from '@blueprintjs/core';
+import { type ChangeProposal } from '../../types/proposal';
+
+
+/** Returns props for a Tag that classifies this proposal. */
+export function proposalToTagProps(proposal: ChangeProposal): TagProps {
+  return {
+    children: proposal.type === 'amendment'
+      ? proposal.amendmentType
+      : proposal.type === 'addition'
+        ? "addition"
+        : proposal.type === 'clarification'
+          ? "clarification"
+          : "(unknown proposal type)",
+    intent: proposal.type === 'amendment'
+      ? 'warning'
+      : proposal.type === 'addition' || proposal.type === 'clarification'
+        ? 'primary'
+        : undefined,
+  }
+}
