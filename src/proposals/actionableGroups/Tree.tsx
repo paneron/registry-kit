1a7ce53f38bf3ca51bbe257f20bfb3a139d0dad2
Anton <anton@strogonoff.name>
2024-03-24 17:56:31 +0800

    refactor(cr): new actionable groups tree view

---
+++ src/proposals/actionableGroups/Tree.tsx
@@ -0,0 +1,191 @@
+/** @jsx jsx */
+/** @jsxFrag React.Fragment */
+
+import React, { useContext, useMemo } from 'react';
+import update from 'immutability-helper';
+import { jsx } from '@emotion/react';
+import { Tree } from '@blueprintjs/core';
+import { DatasetContext } from '@riboseinc/paneron-extension-kit';
+import type { PersistentStateReducerHook } from '@riboseinc/paneron-extension-kit/usePersistentStateReducer';
+import type { SomeCR as CR } from '../../types/cr';
+import { ChangeRequestContext } from '../../views/change-request/ChangeRequestContext';
+import { BrowserCtx } from '../../views/BrowserCtx';
+import { getActionableProposalGroupsForRoles } from './queries';
+import {
+  type ActionableProposalTreeNode,
+  getMapReduceChainsForActionableProposalGroups,
+  getActionableProposalGroupsAsTreeNodes,
+} from './treeNodes';
+
+
+interface State {
+  //selectedFolderID: string | null
+  selectedItemID: string | null
+  expandedFolderIDs: readonly string[]
+}
+
+const initialState: State = {
+  //selectedFolderID: null,
+  selectedItemID: null,
+  expandedFolderIDs: [],
+} as const;
+
+type Action =
+  | { type: 'select-folder'; payload: { folderID: string | null; }; }
+  | { type: 'enter-folder'; payload: { folderID: string; }; }
+  | { type: 'exit-folder'; payload: { folderID: string; }; }
+  | { type: 'select-item'; payload: { itemID: string | null; }; }
+
+
+const ActionableCRTree: React.FC<{
+  className?: string
+}> =
+function ({ className }) {
+  const { changeRequest: activeCR } = useContext(ChangeRequestContext);
+  const { setActiveChangeRequestID, stakeholder } = useContext(BrowserCtx);
+  const { usePersistentDatasetStateReducer, useMapReducedData } = useContext(DatasetContext);
+
+  const [ state, dispatch, ] = (usePersistentDatasetStateReducer as PersistentStateReducerHook<State, Action>)(
+    'actionable-proposals',
+    undefined,
+    undefined,
+    (prevState, action) => {
+      switch (action.type) {
+        case 'select-folder':
+          if (prevState.selectedItemID !== action.payload.folderID) {
+            return {
+              ...prevState,
+              selectedItemID: action.payload.folderID,
+            };
+          } else {
+            return prevState;
+          }
+        case 'enter-folder':
+          if (!prevState.expandedFolderIDs.includes(action.payload.folderID)) {
+            return {
+              ...prevState,
+              selectedItemID: action.payload.folderID,
+              expandedFolderIDs: update(
+                prevState.expandedFolderIDs,
+                { $push: [action.payload.folderID] }),
+            };
+          } else {
+            return prevState;
+          }
+        case 'exit-folder':
+          const idx = prevState.expandedFolderIDs.indexOf(action.payload.folderID);
+          if (idx >= 0) {
+            return {
+              ...prevState,
+              selectedItemID: action.payload.folderID,
+              expandedFolderIDs: update(
+                prevState.expandedFolderIDs,
+                { $splice: [[idx, 1]] }),
+            };
+          } else {
+            return prevState;
+          }
+        case 'select-item':
+          if (prevState.selectedItemID !== action.payload.itemID) {
+            return {
+              ...prevState,
+              selectedItemID: action.payload.itemID,
+            };
+          } else {
+            return prevState;
+          }
+        default:
+          throw new Error("Unexpected browse state");
+      }
+    },
+    initialState,
+    null);
+
+  const groups = getActionableProposalGroupsForRoles(stakeholder?.roles ?? []);
+
+  const chains = stakeholder
+    ? getMapReduceChainsForActionableProposalGroups(groups, stakeholder)
+    : {};
+
+  const actionableProposalsResult = useMapReducedData({ chains });
+
+  const actionableProposals = useMemo(
+    (() =>
+    Object.entries(actionableProposalsResult.value).
+      map(([chainID, chainResult]) => ({
+        groupLabel: chainID,
+        proposals: (Array.isArray(chainResult)
+          // TODO: Validate results
+          ? (chainResult as CR[])
+          : undefined) || undefined
+      }))
+    ),
+    [actionableProposalsResult.value]);
+
+  const nodes = useMemo(
+    (() =>
+      getActionableProposalGroupsAsTreeNodes(actionableProposals, {
+        activeCRID: activeCR?.id,
+        expandedGroupLabels: new Set(state.expandedFolderIDs),
+        selectedGroup: state.selectedItemID
+          ? actionableProposals.
+            find(({ groupLabel }) => groupLabel === state.selectedItemID)
+              ? state.selectedItemID
+              : undefined
+          : undefined,
+        selectedCRID: state.selectedItemID
+          ? actionableProposals.
+            flatMap(({ proposals }) => proposals ?? []).
+            find(p => p.id === state.selectedItemID)
+              ? state.selectedItemID
+              : undefined
+          : undefined,
+      })
+    ),
+    [activeCR?.id, actionableProposals, state.selectedItemID, state.expandedFolderIDs.join(',')]);
+
+  const eventHandlers = useMemo((() => ({
+    onNodeClick: (node: ActionableProposalTreeNode) =>
+      node.nodeData?.type === 'group'
+        ? dispatch({
+            type: 'select-folder',
+            payload: { folderID: node.id as string },
+          })
+        : dispatch({
+            type: 'select-item',
+            payload: { itemID: node.id as string },
+          }),
+    onNodeExpand: (node: ActionableProposalTreeNode) =>
+      node.nodeData?.type === 'group'
+        ? dispatch({
+            type: 'enter-folder',
+            payload: { folderID: node.id as string },
+          })
+        : void 0,
+    onNodeCollapse: (node: ActionableProposalTreeNode) =>
+      node.nodeData?.type === 'group'
+        ? dispatch({
+            type: 'exit-folder',
+            payload: { folderID: node.id as string },
+          })
+        : void 0,
+    onNodeDoubleClick: (node: ActionableProposalTreeNode) =>
+      node.nodeData?.type === 'group'
+        ? dispatch({
+            type: 'enter-folder',
+            payload: { folderID: node.id as string },
+          })
+        // TODO: Do something like switch to home tab or proposal tab?
+        : node.id === activeCR?.id
+          ? setActiveChangeRequestID?.(null)
+          : setActiveChangeRequestID?.(node.id as string),
+  })), [dispatch, activeCR?.id]);
+
+  return <Tree
+    className={className}
+    contents={nodes}
+    {...eventHandlers}
+  />;
+}
+
+export default ActionableCRTree;
---
+++ src/proposals/actionableGroups/treeNodes.tsx
@@ -0,0 +1,109 @@
+/** @jsx jsx */
+/** @jsxFrag React.Fragment */
+
+/**
+ * Tools for rendering proposal groups as tree nodes.
+ * Each proposal group is top-level node, with proposals as nested nodes.
+ */
+
+//import React, { useContext, useMemo } from 'react';
+import { jsx } from '@emotion/react';
+
+import { Tag } from '@blueprintjs/core';
+import type { IconName, TreeNodeInfo } from '@blueprintjs/core';
+import type { RegisterStakeholder } from '../../types';
+import type { SomeCR as CR } from '../../types/cr';
+import type { ActionableProposalGroup } from './types';
+import { CR_BASE_QUERY } from './queries';
+
+
+export type ActionableProposalTreeNode = TreeNodeInfo<{ type: 'group' | 'item' }>;
+
+
+interface ActionableProposalGroupResult {
+  groupLabel: string
+  proposals: CR[] | undefined
+}
+export function getActionableProposalGroupsAsTreeNodes(
+  groups: readonly ActionableProposalGroupResult[],
+  opts?: {
+    expandedGroupLabels?: Set<string>
+    selectedGroup?: string
+    selectedCRID?: string
+    activeCRID?: string
+  },
+): ActionableProposalTreeNode[] {
+  return groups.
+    //filter(({ proposals }) => proposals && proposals.length > 0).
+    map(({ groupLabel, proposals }) => {
+      const hasProposals = proposals && proposals.length > 0;
+      const hasActiveProposal = hasProposals
+        && opts?.activeCRID && proposals.find(p => p.id === opts.activeCRID);
+      const hasSelectedProposal = hasProposals
+        && opts?.selectedCRID && proposals.find(p => p.id === opts.selectedCRID);
+      const isExpanded =
+        groups.length === 1
+        || opts?.expandedGroupLabels?.has(groupLabel)
+        || hasSelectedProposal
+          ? true
+          : false;
+
+      return {
+        id: groupLabel,
+        label: groupLabel,
+        hasCaret: true,
+        secondaryLabel:
+          hasActiveProposal && !isExpanded
+            ? activeCRMarker
+            : <Tag
+                  minimal={!hasProposals}
+                  intent={hasProposals ? 'primary' : undefined}>
+                {hasProposals ? proposals.length : 0}
+              </Tag>,
+        icon: isExpanded
+          ? 'folder-open'
+          : 'folder-close' as IconName,
+        isExpanded,
+        isSelected: groupLabel === opts?.selectedGroup,
+        nodeData: { type: 'group' },
+        childNodes: hasProposals && isExpanded
+          ? proposals.map(p => getActionableProposalTreeNode(p, {
+              isSelected: p.id === opts?.selectedCRID,
+              isActive: p.id === opts?.activeCRID,
+            }))
+          : [],
+      };
+    });
+}
+
+const activeCRMarker = <Tag intent='danger'>active</Tag>;
+
+function getActionableProposalTreeNode(
+  proposal: CR,
+  opts?: { isSelected?: boolean, isActive?: boolean },
+): ActionableProposalTreeNode {
+  return {
+    id: proposal.id,
+    label: proposal.justification,
+    isSelected: opts?.isSelected ?? false,
+    icon: 'lightbulb',
+    secondaryLabel: opts?.isActive ? activeCRMarker : undefined,
+    nodeData: { type: 'item' },
+  };
+}
+
+
+export function getMapReduceChainsForActionableProposalGroups(
+  proposalGroups: readonly ActionableProposalGroup[],
+  stakeholder: RegisterStakeholder,
+) {
+  return proposalGroups.map(([label, , queryGetter]) => {
+    const query = queryGetter(stakeholder);
+    const predicateFunc = `
+      const objPath = key, obj = value;
+      return ((${CR_BASE_QUERY}) && (${query}));
+    `;
+    const mapFunc = `emit(value);`;
+    return { [label]: { mapFunc, predicateFunc } };
+  }).reduce((prev, curr) => ({ ...prev, ...curr }), {});
+}
