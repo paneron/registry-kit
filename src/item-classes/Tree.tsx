9fd78cd6d891e9dbd901cf8d074fd2081fc4f34c
Anton <anton@strogonoff.name>
2024-03-25 14:44:54 +0800

    feat: new item class tree, supporting class groups

---
+++ src/item-classes/Tree.tsx
@@ -0,0 +1,84 @@
+/** @jsx jsx */
+/** @jsxFrag React.Fragment */
+
+import React, { useContext, useMemo, useCallback } from 'react';
+import { jsx } from '@emotion/react';
+
+import { DatasetContext } from '@riboseinc/paneron-extension-kit/context';
+import { TabbedWorkspaceContext } from '@riboseinc/paneron-extension-kit/widgets/TabbedWorkspace/context';
+
+import GenericStatefulTree, { type State as TreeState } from '../views/StatefulTree';
+import { ChangeRequestContext } from '../views/change-request/ChangeRequestContext';
+import { updateCRObjectChangeset } from '../views/change-request/objectChangeset';
+import { BrowserCtx } from '../views/BrowserCtx';
+import type { RegisterItem, InternalItemReference } from '../types';
+import { itemRefToItemPath } from '../views/itemPathUtils';
+import { Protocols } from '../views/protocolRegistry';
+
+import { getMaybeGroupedItemClassesAsTreeNodes } from './treeNodes';
+
+
+const ItemClassTree: React.VoidFunctionComponent<Record<never, never>> = function () {
+
+  const { performOperation, updateObjects, makeRandomID } = useContext(DatasetContext);
+  const { spawnTab } = useContext(TabbedWorkspaceContext);
+  const { changeRequest: activeCR, canEdit: activeCRIsEditable } = useContext(ChangeRequestContext);
+  const { subregisters, itemClasses, itemClassGroups } = useContext(BrowserCtx);
+
+  const createItem = useCallback(async function _createItem(classID: string, subregisterID?: string) {
+    if (!updateObjects || !makeRandomID || !activeCRIsEditable || !activeCR) {
+      throw new Error("Unable to create item: likely current proposal is not editable or dataset is read-only");
+    }
+    if (subregisters && !subregisterID) {
+      throw new Error("Unable to create item: register uses subregisters, but subregister ID was not provided");
+    }
+    const clsConfig = itemClasses[classID];
+    if (!clsConfig) {
+      throw new Error("Unable to generate new item data: item class configuration is missing");
+    }
+    const initialItemData = clsConfig?.defaults ?? {};
+    const itemID = await makeRandomID();
+    const ref: InternalItemReference = { classID, itemID, subregisterID };
+    const registerItem: RegisterItem<any> = {
+      id: itemID,
+      dateAccepted: new Date(),
+      status: 'valid',
+      data: initialItemData,
+    };
+    const itemPath = itemRefToItemPath(ref);
+    await updateObjects({
+      commitMessage: `propose to add new ${ref.classID}`,
+      objectChangeset: updateCRObjectChangeset(
+        activeCR as any,
+        { [itemPath]: { type: 'addition' } },
+        { [itemPath]: registerItem },
+      ),
+      _dangerouslySkipValidation: true,
+    });
+    spawnTab(`${Protocols.ITEM_DETAILS}:${itemRefToItemPath(ref, activeCR.id)}`);
+  }, [activeCR, activeCRIsEditable, subregisters === undefined, spawnTab, updateObjects, makeRandomID]);
+
+  const handleAdd: undefined | ((clsID: string) => Promise<void>) = useMemo((() =>
+    !subregisters && activeCRIsEditable && performOperation
+      ? (clsID) => performOperation('generating new item', createItem)(clsID)
+      : undefined
+  ), [createItem, subregisters === undefined, activeCRIsEditable, performOperation]);
+
+  const getNodes = useCallback(
+    ((state: TreeState) =>
+      getMaybeGroupedItemClassesAsTreeNodes(itemClasses, itemClassGroups, {
+        selectedItemID: state.selectedItemID ?? undefined,
+        expandedGroupLabels: new Set(state.expandedItemIDs),
+        onProposeItem: handleAdd,
+      })),
+    [itemClasses, itemClassGroups, handleAdd],
+  );
+
+  return <GenericStatefulTree
+    getNodes={getNodes}
+    stateKey="item-browser"
+  />;
+};
+
+
+export default ItemClassTree;
---
+++ src/item-classes/treeNodes.tsx
@@ -0,0 +1,81 @@
+/** @jsx jsx */
+/** @jsxFrag React.Fragment */
+
+/**
+ * Tools for rendering proposal groups as tree nodes.
+ * Each proposal group is top-level node, with proposals as nested nodes.
+ */
+
+//import React, { useContext, useMemo } from 'react';
+import { jsx } from '@emotion/react';
+import type { RegistryViewProps } from '../types/views';
+import type { TreeNodeInfo, IconName } from '@blueprintjs/core';
+import { MoreMenu, ItemClassMenu } from '../views/util';
+
+
+export type ItemOrGroupTreeNode = TreeNodeInfo<{ type: 'group' | 'item' }>;
+
+
+export function getMaybeGroupedItemClassesAsTreeNodes<T extends RegistryViewProps>(
+  itemClasses: T['itemClassConfiguration'],
+  itemClassGroups: T['itemClassGroups'] | undefined,
+  opts?: {
+    expandedGroupLabels?: Set<string>
+    selectedItemID?: string
+    onProposeItem?: (clsID: string) => Promise<void>
+  },
+): ItemOrGroupTreeNode[] {
+  if (!itemClassGroups) {
+    return getItemClassesAsTreeNodes(itemClasses, Object.keys(itemClasses));
+  } else {
+    return Object.entries(itemClassGroups).map(([groupLabel, clsIDs]) => {
+      const hasSelectedClass = opts?.selectedItemID && clsIDs.includes(opts.selectedItemID);
+      const isSelected = opts?.selectedItemID && groupLabel === opts.selectedItemID
+        ? true
+        : false;
+      const isExpanded = hasSelectedClass || opts?.expandedGroupLabels?.has(groupLabel);
+      return {
+        id: groupLabel,
+        label: groupLabel,
+        hasCaret: true,
+        isSelected,
+        isExpanded,
+        icon: isExpanded ? 'folder-open' : 'folder-close' as IconName,
+        childNodes: isExpanded
+          ? getItemClassesAsTreeNodes(itemClasses, clsIDs, opts)
+          : [],
+        nodeData: { type: 'group' },
+      };
+    });
+  }
+}
+
+function getItemClassesAsTreeNodes<T extends RegistryViewProps['itemClassConfiguration']>(
+  itemClasses: T,
+  /** Used for ordering. */
+  classIDs: readonly (keyof T)[],
+  opts?: {
+    selectedItemID?: string
+    onProposeItem?: (clsID: string) => Promise<void>
+  },
+): TreeNodeInfo<{ type: 'item' }>[] {
+  return classIDs.map(clsID => {
+    const cls = itemClasses[clsID];
+    return {
+      id: clsID as string,
+      label: cls?.meta.title ?? 'unknown class',
+      icon: 'folder-close',
+      isSelected: opts?.selectedItemID === clsID,
+      nodeData: { type: 'item' },
+      secondaryLabel:
+        <MoreMenu>
+          <ItemClassMenu
+            cfg={cls}
+            onCreate={opts?.onProposeItem ?
+              () => opts.onProposeItem!(clsID as string)
+              : undefined}
+          />
+        </MoreMenu>,
+    };
+  });
+}
