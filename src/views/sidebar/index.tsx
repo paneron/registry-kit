ae09fe24ec52ce1d17738f53225fc90e724bedb6
Anton <anton@strogonoff.name>
2024-03-26 16:24:38 +0800

    feat: new sidebar (WIP)

--- src/views/sidebar/index.tsx
+++ src/views/sidebar/index.tsx
@@ -8,18 +8,18 @@ import { Icon } from '@blueprintjs/core';
 import type { SuperSidebarConfig } from '@riboseinc/paneron-extension-kit/widgets/TabbedWorkspace/types';
 import HelpTooltip from '@riboseinc/paneron-extension-kit/widgets/HelpTooltip';
 
-import Browse from './Browse';
-import Search from './Search';
-import { PendingChangeRequestsBlock, ChangeRequestHistoryBlock } from './Registration';
-import { ExportOptions, ImportOptions } from './ExportImport';
 //import Browse from './Browse';
 import ItemClassTree from '../../item-classes/Tree';
+//import Search from './Search';
+import { ChangeRequestHistoryBlock } from './Registration';
+import ActionableCRTree from '../../proposals/actionableGroups/Tree';
+//import { ExportOptions, ImportOptions } from './ExportImport';
 
 
 export const sidebarIDs = [
   'Browse',
-  'Registration',
-  'Export',
+  //'Registration',
+  //'Export',
 ] as const;
 
 export type SidebarID = typeof sidebarIDs[number];
@@ -27,60 +27,66 @@ export type SidebarID = typeof sidebarIDs[number];
 export const sidebarConfig: SuperSidebarConfig<typeof sidebarIDs> = {
   Browse: {
     icon: () => <Icon icon="list" />,
-    title: "Register items",
+    title: "Browse",
     blocks: [{
       key: 'browse',
       title: <div css={css`display: flex; justify-content: space-between`}>
-        Preset searches
+        Item classes
         <HelpTooltip content="Browse register items by pre-made categories" />
       </div>,
       content: <ItemClassTree />,
       nonCollapsible: false,
       // These have to have height specified due to absolute positioning.
       height: 400,
+    //}, {
+    //  key: 'search',
+    //  title: "New search",
+    //  content: <Search css={css`position: absolute; inset: 0;`} />,
+    //  nonCollapsible: false,
+    //  height: 400,
     }, {
-      key: 'search',
-      title: "New search",
-      content: <Search css={css`position: absolute; inset: 0;`} />,
-      nonCollapsible: false,
-      height: 400,
-    }],
-  },
-  Registration: {
-    icon: () => <Icon icon="lightbulb" />,
-    title: "Proposals",
-    blocks: [{
-      key: 'pending-crs',
-      title: <div css={css`display: flex; justify-content: space-between`}>
-        Pending proposals
-        <HelpTooltip content="Proposals pending decision. If a register item is selected, only proposals affecting that item are shown." />
-      </div>,
-      content: <PendingChangeRequestsBlock />,
-      height: 300,
-    }, {
-      key: 'cr-history',
-      title: <div css={css`display: flex; justify-content: space-between`}>
-        History
-        <HelpTooltip content="Resolved proposals. If a register item is selected, only proposals affecting that item are shown." />
-      </div>,
+      key: 'proposal-history',
+      title: "Proposal history",
       content: <ChangeRequestHistoryBlock />,
-      height: 300,
-    }],
-  },
-  Export: {
-    icon: () => <Icon icon="changes" />,
-    title: "Import and export options",
-    blocks: [{
-      key: 'export',
-      title: "Export options",
-      content: <ExportOptions />,
-      nonCollapsible: true,
-    }, {
-      key: 'import',
-      title: "Import options",
-      content: <ImportOptions />,
       nonCollapsible: false,
-      collapsedByDefault: true,
+      height: 200,
     }],
   },
+  // Registration: {
+  //   icon: () => <Icon icon="lightbulb" />,
+  //   title: "Proposals",
+  //   blocks: [{
+  //     key: 'pending-crs',
+  //     title: <div css={css`display: flex; justify-content: space-between`}>
+  //       Pending proposals
+  //       <HelpTooltip content="Proposals pending decision. If a register item is selected, only proposals affecting that item are shown." />
+  //     </div>,
+  //     content: <PendingChangeRequestsBlock />,
+  //     height: 300,
+  //   }, {
+  //     key: 'cr-history',
+  //     title: <div css={css`display: flex; justify-content: space-between`}>
+  //       History
+  //       <HelpTooltip content="Resolved proposals. If a register item is selected, only proposals affecting that item are shown." />
+  //     </div>,
+  //     content: <ChangeRequestHistoryBlock />,
+  //     height: 300,
+  //   }],
+  // },
+  // Export: {
+  //   icon: () => <Icon icon="changes" />,
+  //   title: "Import and export options",
+  //   blocks: [{
+  //     key: 'export',
+  //     title: "Export options",
+  //     content: <ExportOptions />,
+  //     nonCollapsible: true,
+  //   }, {
+  //     key: 'import',
+  //     title: "Import options",
+  //     content: <ImportOptions />,
+  //     nonCollapsible: false,
+  //     collapsedByDefault: true,
+  //   }],
+  // },
 };
