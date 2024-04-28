5271874f0d1c2c9c67bc20ab185ecb66a2745939
Anton <anton@strogonoff.name>
2024-04-29 01:59:09 +0800

    feat(proposals): show previous proposal details in summary
    a.k.a. register version

--- src/proposals/MetaProperties.tsx
+++ src/proposals/MetaProperties.tsx
@@ -1,17 +1,21 @@
 /** @jsx jsx */
 /** @jsxFrag React.Fragment */
 
-import React from 'react';
+import React, { useContext } from 'react';
 import { jsx, css } from '@emotion/react';
 import { Tag } from '@blueprintjs/core';
 
 import HelpTooltip from '@riboseinc/paneron-extension-kit/widgets/HelpTooltip';
+import { DatasetContext } from '@riboseinc/paneron-extension-kit/context';
 
 import type { Register } from '../types/register';
 import { RegisterStakeholderListItem } from '../views/RegisterStakeholder';
 import type { RegisterStakeholder } from '../types/stakeholder';
+import { crIDToCRPath } from '../views/itemPathUtils';
+import useLatestAcceptedProposal from '../views/hooks/useLatestAcceptedProposal';
 
-import { isCreatedBy, type SomeCR } from './types';
+import ProposalAsListItem from './ListItem';
+import { hasSubmitterInput, isCreatedBy, type SomeCR } from './types';
 
 
 /** Proposal meta properties, must be nested within a DL. */
@@ -22,6 +26,55 @@ const Summary: React.FC<{
 }> = function ({ cr, currentStakeholder, registerMetadata }) {
   const crStakeholder = (registerMetadata?.stakeholders ?? []).
     find(s => s.gitServerUsername === cr.submittingStakeholderGitServerUsername);
+
+  const { useObjectData } = useContext(DatasetContext);
+
+  let latestAcceptedProposalID: string | null | undefined;
+  try {
+    latestAcceptedProposalID = useLatestAcceptedProposal()?.id ?? null;
+  } catch (e) {
+    console.error("Failed to obtain latest accepted proposal");
+    latestAcceptedProposalID = undefined;
+  }
+
+  const crPath = crIDToCRPath(cr.registerVersion);
+  const previousProposal = useObjectData({
+    objectPaths: crPath ? [crPath] : [],
+    nounLabel: 'proposal(s)',
+  }).value?.data[crPath ?? ''] as unknown ?? (crPath ? undefined : null);
+  const versionView = previousProposal && hasSubmitterInput(previousProposal)
+    ? <>
+        <ProposalAsListItem proposal={previousProposal as SomeCR} />
+        {latestAcceptedProposalID
+          ? <>
+              &ensp;
+              {cr.registerVersion === latestAcceptedProposalID
+                ? <Tag css={css`display: inline;`} intent='success' minimal round>
+                    current
+                    {" "}
+                    <HelpTooltip intent='success' content={<>
+                      Published version of the register
+                      {" "}
+                      had not changed since this proposal started.
+                    </>} />
+                  </Tag>
+                : <Tag css={css`display: inline;`} intent='warning' minimal round>
+                    not current
+                    {" "}
+                    <HelpTooltip intent='warning' icon='warning-sign' content={<>
+                      Register is currently at version <strong>{latestAcceptedProposalID}</strong>,
+                      {" "}
+                      which is different from version proposal author may have had in mind.
+                      {" "}
+                      It is recommended that proposed changes are reviewed to avoid unintentionally
+                      {" "}
+                      undoing a prior change.
+                    </>} />
+                  </Tag>}
+            </>
+          : null}
+      </>
+    : cr.registerVersion ?? 'N/A';
 
   return (
     <>
@@ -44,32 +97,8 @@ const Summary: React.FC<{
         <dt>
           Register&nbsp;version before&nbsp;proposal:
         </dt>
-        <dd css={css`white-space: nowrap;`}>
-          {cr.registerVersion ?? 'N/A'}
-          &ensp;
-          {cr.registerVersion === registerMetadata?.version?.id
-            ? <Tag css={css`display: inline;`} intent='success' minimal round>
-                current
-                {" "}
-                <HelpTooltip intent='success' content={<>
-                  Published version of the register
-                  {" "}
-                  had not changed since this proposal started.
-                </>} />
-              </Tag>
-            : <Tag css={css`display: inline;`} intent='warning' minimal round>
-                not current
-                {" "}
-                <HelpTooltip intent='warning' icon='warning-sign' content={<>
-                  Register is currently at version <strong>{registerMetadata?.version?.id ?? 'N/A'}</strong>,
-                  {" "}
-                  which is different from version proposal author may have had in mind.
-                  {" "}
-                  It is recommended that proposed changes are reviewed to avoid unintentionally
-                  {" "}
-                  undoing a prior change.
-                </>} />
-              </Tag>}
+        <dd>
+          {versionView}
         </dd>
       </div>
 
