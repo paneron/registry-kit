15ff7144277ce72f79f869c3e4fbf25c883ff817
Anton <anton@strogonoff.name>
2024-04-29 15:07:45 +0800

    style(proposals): fix unnecessarily long relative import

--- src/proposals/actionableGroups/Tree.tsx
+++ src/proposals/actionableGroups/Tree.tsx
@@ -8,7 +8,7 @@ import { Tree } from '@blueprintjs/core';
 import { DatasetContext } from '@riboseinc/paneron-extension-kit';
 import { TabbedWorkspaceContext } from '@riboseinc/paneron-extension-kit/widgets/TabbedWorkspace/context';
 import type { PersistentStateReducerHook } from '@riboseinc/paneron-extension-kit/usePersistentStateReducer';
-import type { SomeCR as CR } from '../../proposals/types';
+import type { SomeCR as CR } from '../types';
 import { ChangeRequestContext } from '../ChangeRequestContext';
 import { BrowserCtx } from '../../views/BrowserCtx';
 import { getActionableProposalGroupsForRoles } from './queries';
