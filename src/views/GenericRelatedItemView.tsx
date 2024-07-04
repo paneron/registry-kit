b6c86d3018478a677b816f7268a0f1de2d89cb46
Anton <anton@strogonoff.name>
2024-07-04 16:42:23 +0800

    style(minor): break a longer line

--- src/views/GenericRelatedItemView.tsx
+++ src/views/GenericRelatedItemView.tsx
@@ -230,7 +230,13 @@ export const GenericRelatedItemView: React.FC<GenericRelatedItemViewProps & {
           }
         `}
         leftElement={canJump
-          ? <a onClick={jump} css={css`display: flex; flex-flow: row nowrap; align-items: baseline;`}>
+          ? <a
+                onClick={jump}
+                css={css`
+                  display: flex;
+                  flex-flow: row nowrap;
+                  align-items: baseline;
+                `}>
               {itemView}
             </a>
           : itemView}
